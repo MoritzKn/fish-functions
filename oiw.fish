908671b432320fa6c50e8b2ea08c1f10d447eea2
MoritzKn <moritz.kneilmann@gmx.de>
2016-10-05 23:07:28 +0200

    Change OIWs maxdepth to 3

--- oiw.fish
+++ oiw.fish
@@ -1,8 +1,9 @@
 function oiw --description 'Find a folder in your workspace and cd into it'
 	set where ~
+    
     set level 1
     set query "/workspace*$argv"
-    set res (find -L $where -mindepth $level -maxdepth (math $level+1) -type d -iwholename "*$query*" -print)
+    set res (find -L $where -mindepth $level -maxdepth (math $level+2) -type d -iwholename "*$query*" -print)
     set shortest (echo $res | sed -e 's/\s\+/\n/g'| awk '{ print length($0) " " $0; }' | sort -n | cut -d ' ' -f 2- | head -1)
     cd $shortest
 end
