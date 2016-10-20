dcb498730f909af8af11fa45f3bfd06ca7470df8
MoritzKn <moritz.kneilmann@gmx.de>
2016-10-20 09:13:56 +0200

    Remove `sid`

--- sid.fish
+++
@@ -1,3 +0,0 @@
-function sid --description 'Some day I did -- Show what you did in the current git repo since a specific day'
-	git log --author=(git config user.name) --since=$argv[1] --oneline | sed -e 's/[a-z0-9]*/-/'
-end
