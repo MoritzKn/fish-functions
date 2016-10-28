4628576246c94425e185af6020d53b624e569f3a
MoritzKn <moritz.kneilmann@gmx.de>
2016-10-28 09:55:15 +0200

    Increase search depth of `oiw` and exclude some paths to make it more performant

--- oiw.fish
+++ oiw.fish
@@ -1,9 +1,13 @@
 function oiw --description 'Find a folder in your workspace and cd into it'
-	set where ~
-    
-    set level 1
-    set query "/workspace*$argv"
-    set res (find -L $where -mindepth $level -maxdepth (math $level+2) -type d -iwholename "*$query*" -print)
-    set shortest (echo $res | sed -e 's/\s\+/\n/g'| awk '{ print length($0) " " $0; }' | sort -n | cut -d ' ' -f 2- | head -1)
+	set where $HOME
+    set query "$HOME/workspace*$argv*"
+
+    set shortest (find -L $where -mindepth 1 -maxdepth 5 -type d \( -name 'node_modules' -o -name '.git' \) -prune -o -iwholename $query -print \
+    | sed -e 's/\s\+/\n/g' \
+    | awk '{ print length($0) " " $0; }' \
+    | sort -n \
+    | cut -d ' ' -f 2- \
+    | head -1)
+    echo "cd $shortest"
     cd $shortest
 end
