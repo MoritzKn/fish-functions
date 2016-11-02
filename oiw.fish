233a5f1d2527e54a711bda1811a998740845c048
MoritzKn <moritz.kneilmann@gmx.de>
2016-11-02 09:35:58 +0100

    Cleanup oiw

--- oiw.fish
+++ oiw.fish
@@ -2,12 +2,11 @@ function oiw --description 'Find a folder in your workspace and cd into it'
 	set where $HOME
     set query "$HOME/workspace*$argv*"
 
-    set shortest (find -L $where -mindepth 1 -maxdepth 5 -type d \( -name 'node_modules' -o -name '.git' \) -prune -o -iwholename $query -print \
-    | sed -e 's/\s\+/\n/g' \
+    set shortest (find $where -mindepth 1 -maxdepth 5 -type d \( -name 'node_modules' -o -name '.git' \) -prune -o -iwholename $query -print \
     | awk '{ print length($0) " " $0; }' \
     | sort -n \
     | cut -d ' ' -f 2- \
     | head -1)
-    echo "cd $shortest"
+
     cd $shortest
 end
