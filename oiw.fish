337121a2b5b7d6ad80b76fa7adb32ecad385e732
MoritzKn <moritz.kneilmann@gmx.de>
2017-02-24 18:52:27 +0100

    Update oiw

--- oiw.fish
+++ oiw.fish
@@ -1,12 +1,37 @@
-function oiw --description 'Find a folder in your workspace and cd into it'
-	set where $HOME
-    set query "$HOME/workspace*$argv*"
+function oiw --description 'Find a project/folder in your workspace and cd into it'
+	function find_folders
+		find $HOME \
+			-mindepth 1 \
+			-maxdepth 5 \
+			'(' \
+				-name 'node_modules' \
+				-o -name '.git'\
+				-o -name 'src'\
+				-o -name 'res'\
+				-o -name 'lib'\
+				-o -name 'spec'\
+				-o -name 'Assets'\
+			')' \
+			-prune \
+			-o '(' \
+				-type d \
+				'(' \
+					-iwholename "$HOME/workspace*$argv*" \
+					-o -iwholename "$HOME/code/*$argv*" \
+				')' \
+			')' \
+			-print
+	end
 
-    set shortest (find $where -mindepth 1 -maxdepth 5 -type d \( -name 'node_modules' -o -name '.git' \) -prune -o -iwholename $query -print \
-    | awk '{ print length($0) " " $0; }' \
-    | sort -n \
-    | cut -d ' ' -f 2- \
-    | head -1)
+	set shortest ( \
+		find_folders $argv \
+			| awk '{ print length($0) " " $0; }' \
+			| sort -n \
+			| cut -d ' ' -f 2- \
+			| head -1 \
+	)
 
-    cd $shortest
+	cd $shortest
+
+	functions -e findFolders
 end
