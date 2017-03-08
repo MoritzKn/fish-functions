e7630166099391458eca4eccb5a4058a9872cdce
MoritzKn <moritz.kneilmann@gmx.de>
2017-03-08 14:24:58 +0100

    Fix unpack

--- unpack.fish
+++ unpack.fish
@@ -2,13 +2,13 @@ function unpack
 	switch $argv[1]
         case '*.zip'
             unzip $argv
-        case '*.tar.gz'
+        case '*.tar.gz' '*.tgz'
             untargz $argv
-        case '*.tag.bz'
-            untarbz
+        case '*.tar.bz2' '*.tbz' '*.tar.bz'
+            untarbz $argv
         case '*.tar'
             untar $argv
-		case '*.rar'
-			unrar e $argv
+        case '*.rar'
+            unrar e $argv
     end
 end
