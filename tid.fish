c8c2a5d838720a7fd6faa4dabea78d2b4d72f96d
MoritzKn <moritz.kneilmann@gmx.de>
2017-04-04 10:14:08 +0200

    Rework tid

--- tid.fish
+++ tid.fish
@@ -1,3 +1,11 @@
 function tid --description 'Today I did -- Show what you did in the current git reop today'
-	git log --author=(git config user.name) --since=(date -d 'yesterday 13:00 ' '+%Y-%m-%d') --oneline | sed -e 's/[a-z0-9]*/-/'
+    git log --author=(git config user.name) --since="4am" --oneline | sed \
+        -e '/Kommentar/Id' \
+        -e '/comment/Id' \
+        -e '/typo/Id' \
+        -e '/whitespace/Id' \
+        -e '/white space/Id' \
+        -e '/indent/Id' \
+        -e '/Release/Id' \
+        -e 's/[a-z0-9]*/-/'
 end
