c86b101ae72a6407a4898302acde1ea7cb47ae37
MoritzKn <moritz.kneilmann@gmx.de>
2017-05-07 01:00:52 +0200

    Refactor unpack

--- unpack.fish
+++ unpack.fish
@@ -1,14 +1,10 @@
 function unpack
-	switch $argv[1]
-        case '*.zip'
-            unzip $argv
-        case '*.tar.gz' '*.tgz'
-            untargz $argv
-        case '*.tar.bz2' '*.tbz' '*.tar.bz'
-            untarbz $argv
-        case '*.tar'
-            untar $argv
-        case '*.rar'
-            unrar e $argv
+    switch $argv[1]
+    case '*.tgz' '*.tbz' '*.tar' '*.tar.*'
+        untar $argv
+    case '*.zip'
+        unzip $argv
+    case '*.rar'
+        unrar x $argv
     end
 end
