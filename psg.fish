95cdbadf51f51bc0bf9adb22ddaa0a411e42ab1f
MoritzKn <moritz.kneilmann@gmx.de>
2017-08-25 18:57:27 +0200

    Update psg

--- psg.fish
+++ psg.fish
@@ -1,3 +1,8 @@
-function psg --description='grep from ps aux'
-    ps aux | grep $argv
+function psg --description 'grep from ps aux'
+	#ps aux | awk "NR==1 || (/$argv/ && !/awk/)"
+	ps aux | begin
+		read first_line
+		echo $first_line
+		grep -v grep | grep $argv
+	end
 end
