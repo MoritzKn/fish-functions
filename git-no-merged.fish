3b800d7e4ad24a473ef4e24c7f548cd239c8c99f
MoritzKn <moritz.kneilmann@gmx.de>
2017-08-29 16:13:06 +0200

    Fix indent

--- git-no-merged.fish
+++ git-no-merged.fish
@@ -1,5 +1,5 @@
 function git-no-merged --argument branch
-	if test -z "$branch"
+    if test -z "$branch"
         set -l branch (git rev-parse --abbrev-ref HEAD)
     end
     git branch --no-merged $branch
--- psg.fish
+++ psg.fish
@@ -1,8 +1,8 @@
 function psg --description 'grep from ps aux'
-	#ps aux | awk "NR==1 || (/$argv/ && !/awk/)"
-	ps aux | begin
-		read first_line
-		echo $first_line
-		grep -v grep | grep $argv
-	end
+    #ps aux | awk "NR==1 || (/$argv/ && !/awk/)"
+    ps aux | begin
+        read first_line
+        echo $first_line
+        grep -v grep | grep $argv
+    end
 end
