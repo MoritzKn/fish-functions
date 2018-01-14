6375efb8d36328f08419ad27caee86b5ce9f9b20
MoritzKn <moritz.kneilmann@gmx.de>
2018-01-14 02:17:11 +0100

    Update 'do' to be aware of git forks

--- do.fish
+++ do.fish
@@ -25,11 +25,42 @@ function do --description 'Do what I want'
 
     switch "$subject"
         case 'git@*.git' 'https://*.git' 'git://*'
+            set -l repo (string match -r '/([a-zA-Z_-]+)/?(?:\\.git)?$' $subject | sed -n 2p)
+
+            if begin; test -n "$repo"; and string match -e github $subject; end
+                set -l owner (string match -r '(?:/|:)([a-zA-Z_-]+)/[a-zA-Z_-]+/?(?:\\.git)?$' $subject | sed -n 2p)
+                set -l url https://api.github.com/repos/$owner/$repo
+                set -l info (curl $url -s)
+
+                if test -n "$info" -a (echo $info | jq .fork -r) = true
+                    set -l remote (git config --get remote.origin.url)
+                    set -l git_url (echo $info | jq .parent.git_url -r)
+                    set -l ssh_url (echo $info | jq .parent.ssh_url -r)
+                    set -l https_url (echo $info | jq .parent.clone_url -r)
+                    set -l parent_name (echo $info | jq .parent.name -r)
+
+                    if test -z "$remote"
+                        echo "Clone parent ($ssh_url) and add fork ($subject) as remote"
+                        git clone $ssh_url
+                        cd $parent_name
+                        git remote add fork $subject
+                        atom -a .
+                        return
+                    end
+
+                    if test "$remote" = "$git_url" -o "$remote" = "$ssh_url" -o "$remote" = "$https_url"
+                        echo "Add remote: $subject"
+                        git remote add fork $subject
+                        return
+                    end
+                end
+            end
+
             echo "Clone repo: $subject"
             git clone $subject
-            set -l name (string match -r '/([a-zA-Z_-]+)/?(?:\\.git)?$' $subject | sed -n 2p)
-            if test -n $name
-                cd $name
+
+            if test -n "$repo"
+                cd $repo
                 atom -a .
             end
 
