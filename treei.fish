1e7ea472d68417a763a4335ed1d331b04008066e
MoritzKn <moritz.kneilmann@gmx.de>
2018-01-24 23:33:56 +0100

    Fix typo

--- treei.fish
+++ treei.fish
@@ -1,4 +1,4 @@
-function treei --decription 'tree but ignore everything in .gitignore'
+function treei --description 'tree but ignore everything in .gitignore'
     if test -f .gitignore
         tree -I (cat .gitignore | tr '\n' '|' | sed -e 's/|$/\n/' -e 's/\///g') $argv
     else
