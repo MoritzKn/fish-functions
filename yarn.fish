199cabdb09504eb2914a893cacf5a7e2410bb75d
MoritzKn <moritz.kneilmann@gmx.de>
2018-05-15 17:28:54 +0200

    Update yarn wrapper

--- yarn.fish
+++ yarn.fish
@@ -1,11 +1,13 @@
 function yarn
     if test (count $argv) -gt 0
         switch $argv[1]
-            case 'start' 'dev'
+            case 'start' 'dev' 'inspect'
                 iterm-tab-color 36 136 182
         end
     end
 
     command yarn $argv
+    set -l s $status
     iterm-tab-color
+    return $s
 end
