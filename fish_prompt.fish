09f5271aa9b685c063036b5a7278fc2bd21f4873
MoritzKn <moritz.kneilmann@gmx.de>
2019-10-15 16:16:05 +0200

    Add space in prompt before path
    Separates host & path into two tokens, usefull for selecting or clicking on paths

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -43,5 +43,5 @@ function fish_prompt --description 'Prompt ausgeben'
     end
 
     # $USER
-    echo -n -s [(date '+%H:%M')] ' ' $hn (set_color $color_cwd) (prompt_pwd) (set_color normal) $status_warning "$suffix "
+    echo -n -s [(date '+%H:%M')] ' ' $hn ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal) $status_warning "$suffix "
 end
