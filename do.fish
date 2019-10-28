add3ca0a45231cf986bd8d3820cc7790851a586d
MoritzKn <moritz.kneilmann@gmx.de>
2019-10-28 09:48:11 +0100

    Add comments to do script

--- do.fish
+++ do.fish
@@ -1,10 +1,12 @@
 function do --description 'Do what I want'
+    # Create a history entry
     function add_to_history
         set -l cmd (string join '\n' $argv)
         set -l time (date +%s)
         echo "- cmd: $cmd"\n"  when: $time" >> ~/.local/share/fish/fish_history
     end
 
+    # Execute a command and add it to history as if a user would have executed it
     function do_action
         for line in (string split \n $argv)
             eval $line
@@ -23,13 +25,16 @@ function do --description 'Do what I want'
 
         set -l subject "$argv"
 
+        # Get subject from clipboard
         if test -z "$subject"
             if type -q xclip
+                # Xorg clipboard
                 set subject (xclip -o -selection clipboard)
                 if test -z "$subject"
                     set subject (xclip -o)
                 end
             else if type -q pbpaste
+                # MacOS clipboard
                 set subject (pbpaste)
             else
                 echo 'No clipboard utility found'
@@ -45,14 +50,17 @@ function do --description 'Do what I want'
         end
 
         switch "$subject"
+            # Handle GitHub PR
             case 'https://github.com/*/*/pull/*'
                 echo "Checkout PR: $subject"
                 do_action "hub checkout $subject"
                 return
 
+            # Clone git repo
             case 'git@*.git' 'https://*.git' 'git://*' 'https://github.com/*/*'
                 set -l repo (string match -r '/([a-zA-Z_-]+)/?(?:\\.git)?$' $subject | sed -n 2p)
 
+                # Special case for GitHub
                 if begin; test -n "$repo"; and string match -e 'github.com' $subject > /dev/null; end
                     set -l owner (string match -r '(?:/|:)([a-zA-Z_-]+)/[a-zA-Z_-]+/?(?:\\.git)?$' $subject | sed -n 2p)
                     set -l api_url https://api.github.com/repos/$owner/$repo
@@ -61,7 +69,8 @@ function do --description 'Do what I want'
                     if test -n "$info"
                         set -l ssh_url (echo $info | jq .ssh_url -r)
 
-                        if test (echo $info | jq .fork -r) = true
+                        # If the repo is a fork
+                        if test (echo $info | jq .fork -r) = "true"
                             set -l remote (git config --get remote.origin.url)
                             set -l parent_git_url (echo $info | jq .parent.git_url -r)
                             set -l parent_ssh_url (echo $info | jq .parent.ssh_url -r)
@@ -69,6 +78,7 @@ function do --description 'Do what I want'
                             set -l parent_name (echo $info | jq .parent.name -r)
                             set -l add_remote
 
+                            # Clone the original
                             if test -z "$remote"
                                 echo "Clone parent repo: $parent_ssh_url"
                                 do_action "git clone $parent_ssh_url"; or return 1
@@ -82,6 +92,7 @@ function do --description 'Do what I want'
                                 set add_remote yes
                             end
 
+                            # And add the fork as second remote
                             if test -n "$add_remote"
                                 set -l new_remote
                                 if test -n "$ssh_url"
@@ -96,6 +107,7 @@ function do --description 'Do what I want'
                                 return
                             end
                         else if test -n "$ssh_url"
+                            # Clone with the SSH URL if possible
                             echo "Clone repo (ssh): $subject"
                             do_action "git clone $subject"; or return 1
                             do_action "cd $repo"
@@ -105,6 +117,7 @@ function do --description 'Do what I want'
                     end
                 end
 
+                # Do a regular clone of the repo
                 echo "Clone repo: $subject"
                 do_action "git clone $subject"; or return 1
 
@@ -114,33 +127,39 @@ function do --description 'Do what I want'
                 end
                 return
 
+            # Check out branch if it starts with feature
             case 'feature/*'
                 echo "Checkout feature: $subject"
                 do_action "git fetch; git checkout $subject; git pull"
                 return
 
+            # Download a  file
             case 'http://*' 'https://*'
                 echo "Download: $subject"
                 do_action "curl -LO $subject"
                 return
 
-            case '*.tgz' '*.tbz' '*.tar' '*.tar.*' '*.zip' '*.rar' '*gz'
+            # Unpack a file
+            case '*.tgz' '*.tbz' '*.tar' '*.tar.*' '*.zip' '*.rar' '*gz' '*bz2'
                 echo "Unpack: $subject"
                 do_action "unpack $subject"
                 return
 
+            # Open a URL
             case '*://*'
                 echo "Open: $subject"
                 do_action "open $subject"
                 return
         end
 
+        # Resolve ~ for home
         if set -l path (string replace -a '~' ~ $subject)
             if test -e "$path"
                 set subject $path
             end
         end
 
+        # Handle files
         if test -e "$subject"
             if test -d "$subject"
                 echo "Cd into: $subject"
