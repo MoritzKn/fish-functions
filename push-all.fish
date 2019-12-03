ebf9e3c3e1faf56dcde67aa5f6048668828f95b1
Moritz Kneilmann <moritz.kneilmann@gmx.de>
2019-12-03 17:10:54 +0100

    Add function push-all

---
+++ push-all.fish
@@ -0,0 +1,16 @@
+function push-all --description "Pull origin and push all other remotes"
+    set -l diff (git diff --stat)
+    if test "$diff" != ''
+      echo "Working tree not clean"
+    end
+
+    echo "Pull origin"
+    git pull origin
+
+    for remote in (git remote)
+        if test "$remote" != "origin"
+            echo "Push $remote"
+            git push "$remote"
+        end
+    end
+end
