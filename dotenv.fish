3fb31e9a92ddd0193c041cf1ec377ad58a1ba3ea
Moritz Kneilmann <moritz.kneilmann@gmx.de>
2019-12-09 11:07:11 +0100

    Update dotenv to support comments

--- dotenv.fish
+++ dotenv.fish
@@ -4,9 +4,18 @@ function dotenv --description 'Load environment variables from .env file'
     set envfile $argv[1]
   end
 
-  if test -e $envfile
-    for line in (cat $envfile)
-      set -xg (echo $line | cut -d = -f 1) (echo $line | cut -d = -f 2-)
+  if test ! -e $envfile
+      exit 1
+  end
+
+  for line in (cat $envfile)
+    set line (string trim $line)
+
+    if test -n $line; and not string match '#*' $line > /dev/null
+      set -l value (echo $line | cut -d = -f 2-)
+      set -l name (string trim (echo $line | cut -d = -f 1))
+
+      echo set -xg $name $value
     end
   end
 end
