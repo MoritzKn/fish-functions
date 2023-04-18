5f0ec7cec17534135877f4e2bd41264a0f3b558c
Moritz Kneilmann <moritz.kneilmann@gmx.de>
2023-04-18 19:24:55 +0200

    Add vercel-switch.fish

---
+++ vercel-switch.fish
@@ -0,0 +1,18 @@
+function vercel-client
+  echo "====="
+  echo "\$ vercel link -p $argv"
+  vercel link -p $argv
+
+  and echo "====="
+  echo "\$ vercel env pull"
+  vercel env pull
+
+
+  and echo "====="
+  echo "\$ sed -i '/VERCEL_URL=""/d' .env"
+  sed -i '/VERCEL_URL=""/d' .env
+
+  and echo "====="
+  echo "\$ npm run dev"
+  npm run dev
+end
